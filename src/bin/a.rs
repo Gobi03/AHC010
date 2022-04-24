17e327e9071c52de4fc1c6bf8e9a3070312aae05
Tatsuya Katsushima <t.katsushima@opt.ne.jp>
2022-04-24 15:15:39 +0900

    check

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -7,17 +7,50 @@ use std::cmp::*;
 #[allow(unused_imports)]
 use std::collections::*;
 
+#[allow(unused_imports)]
+use rand::rngs::ThreadRng;
+#[allow(unused_imports)]
+use rand::seq::SliceRandom;
+#[allow(unused_imports)]
+use rand::{thread_rng, Rng};
+#[allow(unused_imports)]
+use std::io::Write;
+use std::time::SystemTime;
+
 #[allow(dead_code)]
 const MOD: usize = 1e9 as usize + 7;
 
+const SIDE: usize = 30;
+
+struct Input {
+    t: Vec<Vec<usize>>,
+}
+impl Input {
+    fn new(t: Vec<Vec<char>>) -> Self {
+        Self {
+            t: t.iter()
+                .map(|cs| cs.iter().map(|&c| c as usize - 48).collect())
+                .collect(),
+        }
+    }
+}
+
 #[fastout]
 fn main() {
+    let system_time = SystemTime::now();
+    let mut _rng = thread_rng();
+
     input! {
-        // n: usize,
-        // v: [isize; n],
+        t: [Chars; SIDE],
     }
 
-    let ans = 42;
+    let input = Input::new(t);
 
-    println!("{}", ans);
+    for _ in 0..900 {
+        print!("0")
+    }
+
+    println!("");
+
+    eprintln!("{}ms", system_time.elapsed().unwrap().as_millis());
 }
