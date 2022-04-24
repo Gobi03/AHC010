11b693cb4f20ce82bdf5e6109cfdd79e15fdf5af
Tatsuya Katsushima <t.katsushima@opt.ne.jp>
2022-04-24 14:40:30 +0900

    initial commit

---
+++ src/bin/a.rs
@@ -0,0 +1,23 @@
+#[allow(unused_imports)]
+use proconio::marker::{Chars, Isize1, Usize1};
+use proconio::{fastout, input};
+
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+
+#[allow(dead_code)]
+const MOD: usize = 1e9 as usize + 7;
+
+#[fastout]
+fn main() {
+    input! {
+        // n: usize,
+        // v: [isize; n],
+    }
+
+    let ans = 42;
+
+    println!("{}", ans);
+}
